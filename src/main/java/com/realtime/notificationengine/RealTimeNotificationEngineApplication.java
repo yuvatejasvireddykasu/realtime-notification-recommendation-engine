9530732f2ae1179a18f37d240cf209b0357e572a
Yuvatejasvi Kasu <Kasu.Yuvatejasvi@blueyonder.com>
2025-10-30 11:34:21 +0530

    Initial Spring Boot reactive project setup

---
+++ src/main/java/com/realtime/notificationengine/RealTimeNotificationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.realtime.notificationengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeNotificationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeNotificationEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realtime/notificationengine/RealTimeNotificationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtime.notificationengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeNotificationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
